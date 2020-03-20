5a361de68732cc41b88cd56a171390144fd36e10
‘Juliano <julianoperins@gmail.com>
2020-03-19 21:06:54 -0400

    Added single page user

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,18 +1,35 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Search extends Component {
   state = {
     text: ""
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired
+  };
+
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state.text);
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter something", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
+
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: "" });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -26,9 +43,18 @@ export class Search extends Component {
           <input
             type="submit"
             value="Search"
-            className="btn btn-dark btn-block"
+            className="btn btn-primary btn-block"
           />
         </form>
+        {showClear && (
+          <button
+            onClick={clearUsers}
+            type="submit"
+            className="btn btn-primary btn-block"
+          >
+            Clear
+          </button>
+        )}
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   return (
@@ -8,14 +9,13 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
       <h3>{login}</h3>
 
       <div>
-        <a
-          href={html_url}
-          target="_blank"
+        <Link
+          to={`/user/${login}`}
           className="btn btn-dark btn-sm my-1"
           style={{ borderRadius: "5px" }}
         >
           See Profile
-        </a>
+        </Link>
       </div>
     </div>
   );
